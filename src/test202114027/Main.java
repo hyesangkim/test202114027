34247d1b47656114749fb4753723d3cd4751dec8
hyesangkim <khs1010602@gmail.com>
2025-10-19 21:11:38 +0900

    three

--- src/test202114027/Main.java
+++ src/test202114027/Main.java
@@ -2,5 +2,6 @@ package test202114027;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("김혜상");	}
+		System.out.println("김혜상");
+		World.run()	;}
 }
---
+++ src/test202114027/World.java
@@ -0,0 +1,7 @@
+package test202114027;
+
+public class World{
+    public static void run(){
+        System.out.println("202114027");
+    }
+}
